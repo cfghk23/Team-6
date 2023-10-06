c76d81079bd209e95e7b1666eb2ff6aea2866b68
Masood Ahmed <mangimasood2000@gmail.com>
2023-10-06 15:39:38 +0800

    deleted some files

--- Dashbaord/src/routes.js
+++ Dashbaord/src/routes.js
@@ -11,13 +11,9 @@ import {
 
 // Admin Imports
 import MainDashboard from "views/admin/default";
-import NFTMarketplace from "views/admin/marketplace";
-import Profile from "views/admin/profile";
+// import NFTMarketplace from "views/admin/marketplace";
+// import Profile from "views/admin/profile";
 import DataTables from "views/admin/dataTables";
-import RTL from "views/admin/rtl";
-
-// Auth Imports
-import SignInCentered from "views/auth/signIn";
 
 const routes = [
   {
@@ -27,21 +23,6 @@ const routes = [
     icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
     component: MainDashboard,
   },
-  // {
-  //   name: "NFT Marketplace",
-  //   layout: "/admin",
-  //   path: "/nft-marketplace",
-  //   icon: (
-  //     <Icon
-  //       as={MdOutlineShoppingCart}
-  //       width='20px'
-  //       height='20px'
-  //       color='inherit'
-  //     />
-  //   ),
-  //   component: NFTMarketplace,
-  //   secondary: true,
-  // },
   {
     name: "Data Tables",
     layout: "/admin",
@@ -49,27 +30,6 @@ const routes = [
     path: "/data-tables",
     component: DataTables,
   },
-  // {
-  //   name: "Profile",
-  //   layout: "/admin",
-  //   path: "/profile",
-  //   icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
-  //   component: Profile,
-  // },
-  // {
-  //   name: "Sign In",
-  //   layout: "/auth",
-  //   path: "/sign-in",
-  //   icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
-  //   component: SignInCentered,
-  // },
-  // {
-  //   name: "RTL Admin",
-  //   layout: "/rtl",
-  //   path: "/rtl-default",
-  //   icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-  //   component: RTL,
-  // },
 ];
 
 export default routes;
